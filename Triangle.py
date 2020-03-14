09bf6b6b6791a25ee908aeff5e937de782db437d
Shane McDonough <shanemcdman@gmail.com>
2020-03-14 00:14:59 -0700

    Draw recursive triangle

--- Triangle.py
+++ Triangle.py
@@ -7,3 +7,11 @@ class Triangle:
     
     def draw(self):
         pygame.draw.polygon(self.screen, (255,255,255), self.points, 1)
+        xdist = abs(self.points[0][0] - self.points[1][0])
+        ydist = abs(self.points[0][1] - self.points[1][1])
+        if (xdist + ydist) / 2 > 1:
+            new_points = [0,0,0]
+            new_points[0] = (int((self.points[0][0] + self.points[1][0]) / 2), int((self.points[0][1] + self.points[1][1]) / 2))
+            new_points[1] = (int((self.points[1][0] + self.points[2][0]) / 2), int((self.points[1][1] + self.points[2][1]) / 2))
+            new_points[2] = (int((self.points[2][0] + self.points[0][0]) / 2), int((self.points[2][1] + self.points[0][1]) / 2))
+            Triangle(self.screen, new_points).draw()
