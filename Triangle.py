3640d2b7915a810cf1daa7e6aaccc9e787df3977
Shane McDonough <shanemcdman@gmail.com>
2020-03-13 23:58:15 -0700

    Create draw fn

--- Triangle.py
+++ Triangle.py
@@ -1,8 +1,9 @@
 import pygame
 
 class Triangle:
-    def __init__(self, points):
+    def __init__(self, screen, points):
+        self.screen = screen
         self.points = points
     
     def draw(self):
-        pass
+        pygame.draw.polygon(self.screen, (255,255,255), self.points, 1)
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from Triangle import Triangle
 os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
 pygame.display.init()
-size = (700,700)
+size = (600,600)
 screen = pygame.display.set_mode(size)
-triangle = Triangle(((size[0]/2,0), (size[0],size[1]), (0,size[1])))
+triangle = Triangle(screen, ((size[0]/2,5), (size[0]-5,size[1]-5), (5,size[1]-5)))
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0,0,0))
+    triangle.draw()
     pygame.display.update()
