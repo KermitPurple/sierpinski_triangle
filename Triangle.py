6ad6c3891f3237108ea0b79908e996dceaad00d3
Shane McDonough <shanemcdman@gmail.com>
2020-03-13 23:03:26 -0700

    Create class Triangle

---
+++ Triangle.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Triangle:
+    def __init__(self, points):
+        self.points = points
+    
+    def draw(self):
+        pass
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 import os
+from Triangle import Triangle
 os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
 pygame.display.init()
 size = (700,700)
 screen = pygame.display.set_mode(size)
+triangle = Triangle(((size[0]/2,0), (size[0],size[1]), (0,size[1])))
 running = True
 while running:
     for event in pygame.event.get():
